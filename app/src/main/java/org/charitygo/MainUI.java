8cad214a7ba73809f5c61e1e4fbe34d6d71a43c8
ncct96 <30764188+ncct96@users.noreply.github.com>
2018-11-02 15:48:13 +0800

    Nicholas Test

--- app/src/main/java/org/charitygo/MainUI.java
+++ app/src/main/java/org/charitygo/MainUI.java
@@ -40,10 +40,10 @@ public class MainUI extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ui);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,13 +52,13 @@ public class MainUI extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
 
@@ -94,9 +94,9 @@ public class MainUI extends AppCompatActivity
     @Override
     public void step(long timeNs) {
         numSteps++;
-        progress = (numSteps/10) * 100;
+        progress = (numSteps/100) * 100;
         progressBar.setProgress(progress);
-        txtProgress.setText(TEXT_NUM_STEPS + numSteps + "" + progress);
+        txtProgress.setText(TEXT_NUM_STEPS + numSteps + " " + progress);
 
     }
 
@@ -108,7 +108,7 @@ public class MainUI extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -158,7 +158,7 @@ public class MainUI extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
