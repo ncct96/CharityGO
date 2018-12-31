2ca9dceb5ee3c3f5c230cfc78518edef6bfeeef4
ncct96 <30764188+ncct96@users.noreply.github.com>
2018-12-31 13:57:27 +0800

    Added Setting activity

--- app/src/main/java/org/charitygo/activity/SettingActivity.java
+++ app/src/main/java/org/charitygo/activity/SettingActivity.java
@@ -1,23 +1,40 @@
 package org.charitygo.activity;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Switch;
 
 import org.charitygo.R;
 import org.charitygo.SettingFragment;
 
-public class SettingActivity extends AppCompatActivity {
+import java.util.List;
+
+public class SettingActivity extends PreferenceActivity {
+
+    public static final String KEY_REMINDER = "reminder";
+
+    @Override
+    public void onBuildHeaders(List<Header> target)
+    {
+        loadHeadersFromResource(R.xml.pref_setting, target);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName)
+    {
+        return SettingFragment.class.getName().equals(fragmentName);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingFragment())
-                .commit();
 
     }
+
+
 }
